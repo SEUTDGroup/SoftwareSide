2d30e79e68379c60ff1505971c4b6b537c0b5de3
yd208 <yadiel.graciano@yahoo.com>
2018-02-03 16:01:14 -0600

    First Commit: Start Android App

---
+++ app/src/main/java/com/soft_eng_project/heimdallr/Login.java
@@ -0,0 +1,42 @@
+package com.soft_eng_project.heimdallr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Yadiel on 2/2/2018.
+ */
+
+public class Login extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+}
---
+++ app/src/main/java/com/soft_eng_project/heimdallr/MainActivity.java
@@ -0,0 +1,58 @@
+package com.soft_eng_project.heimdallr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    String tag = "Heimdallr";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent launchLogin = new Intent(this, Login.class);
+        launchLogin.putExtra("Launch Successful", "Message");
+        startActivity(launchLogin);
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(tag, "This is onStart()");
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(tag, "This is onPause()");
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(tag, "This is onStop()");
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(tag, "This is onResume()");
+    }
+
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(tag, "This is onRestart()");
+    }
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(tag, "This is onDestroy");
+    }
+
+}
