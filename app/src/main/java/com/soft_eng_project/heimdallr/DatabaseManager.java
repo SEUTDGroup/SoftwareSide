031e98b01690480e7b2d481debabd03d4afe6963
yd208 <yadiel.graciano@yahoo.com>
2018-02-22 13:08:46 -0600

    Third Commit: Settings
    **********Created***********
    - AccountInfoController class
    - DatabaseManager singleton class
    - User class
    
    AccountInfoController:
    Manages the user information inputted into the Settings and Register gui's by calling DatabaseManger
    
    DatabaseManager:
    Singleton class that interacts with the database by adding, deleting, and updating records in database
    
    User:
    Contains all of the information for a User account

---
+++ app/src/main/java/com/soft_eng_project/heimdallr/DatabaseManager.java
@@ -0,0 +1,48 @@
+package com.soft_eng_project.heimdallr;
+
+/**
+ * Created by Yadiel on 2/22/2018.
+ */
+
+public class DatabaseManager {
+
+    private static DatabaseManager dbManager;
+
+    private DatabaseManager(){}
+
+    //Get DatabaseManger at runtime
+    public static DatabaseManager getDBManager()
+    {
+        if (dbManager == null)
+        {
+            dbManager = new DatabaseManager();
+        }
+        return dbManager;
+    }
+
+    //Prevent DatabaseManger instance from being cloned
+    public Object clone() throws CloneNotSupportedException
+    {
+        throw new CloneNotSupportedException();
+    }
+
+    //Returns a User object from the database
+    public User getUser(String userName)
+    {
+        //do stuff with database
+        return null;
+    }
+
+    //Saves User object information in database
+    public void saveUser(User user)
+    {
+        //do stuff with database
+    }
+
+    //Retrieves User information from database and updates it
+    public void updateUser(User user)
+    {
+        //do stuff with database
+    }
+
+}
---
+++ app/src/main/java/com/soft_eng_project/heimdallr/User.java
@@ -0,0 +1,45 @@
+package com.soft_eng_project.heimdallr;
+
+/**
+ * Created by Yadiel on 2/22/2018.
+ */
+
+public class User {
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+
+    //Default constructor
+    public User() {}
+
+    //User constructor
+    public User(String firstName, String lastName, String userName, String password, String email)
+    {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password  = password;
+        this.email = email;
+    }
+
+    //Updates user information
+    public void updateInfo(String firstName, String lastName, String password, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password  = password;
+        this.email = email;
+    }
+
+    //Getter methods
+    public String getFirstName(){return firstName;}
+    public String getLastName(){return lastName;}
+    public String getUserName(){return userName;}
+    public String getPassword(){return password;}
+    public String getEmail(){return email;}
+
+
+}
